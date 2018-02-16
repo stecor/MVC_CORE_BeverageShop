0879c54efc257fd31691832d3eabd9a4ea9bb9be
Stefano Corra <stefano.corra@hotmail.com>
2018-02-16 00:12:30 -0500

    Shopping cart

---
+++ BeverageAndDrink/Data/Models/ShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeverageAndDrink.Data.Models
+{
+    public class ShoppingCart
+    {
+        public string ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+    }
+}
---
+++ BeverageAndDrink/Data/Models/ShoppingCartItem.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeverageAndDrink.Data.Models
+{
+    public class ShoppingCartItem
+    {
+        public int ShoppingCartItemId { get; set; }
+
+        public Drink Drink { get; set; }
+
+        public int Amount { get; set; }
+
+        public string ShoppingCartId { get; set; }
+
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+    }
+}
